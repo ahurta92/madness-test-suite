41e036203717888631ed7176e925f100f5c601e8
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-08-16 13:29:44 -0400

    Using the right name pCVDZ

--- compute_dalton_basis.py
+++ compute_dalton_basis.py
@@ -9,7 +9,7 @@ mol = sys.argv[1]
 num_proc = sys.argv[2]
 
 basis_list = ['aug-cc-pCVDZ', 'aug-cc-pCVTZ', 'aug-cc-pCVQZ']  # , 'aug-cc-pV5Z', 'aug-cc-pV6Z']
-d_basis_list = ['d-aug-cc-pVCDZ', 'd-aug-cc-pCVTZ', 'd-aug-cc-pCVQZ']  # , 'd-aug-cc-pV5Z', 'd-aug-cc-pV6Z']
+d_basis_list = ['d-aug-cc-pCVDZ', 'd-aug-cc-pCVTZ', 'd-aug-cc-pCVQZ']  # , 'd-aug-cc-pV5Z', 'd-aug-cc-pV6Z']
 
 runner = DaltonRunner()
 
