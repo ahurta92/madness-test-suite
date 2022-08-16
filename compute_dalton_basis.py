19d2ca87c59eb0123b8ad7f8c6a14245667340d8
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-08-16 19:22:13 -0400

    add 5z

--- compute_dalton_basis.py
+++ compute_dalton_basis.py
@@ -1,15 +1,15 @@
 import os
+
 os.chdir("/gpfs/projects/rjh/adrian/post_watoc/august/")
 from madnessReader import *
 from daltonRunner import DaltonRunner
 
-
-
 mol = sys.argv[1]
 num_proc = sys.argv[2]
 
-basis_list = ['aug-cc-pCVDZ', 'aug-cc-pCVTZ', 'aug-cc-pCVQZ']  # , 'aug-cc-pV5Z', 'aug-cc-pV6Z']
-d_basis_list = ['d-aug-cc-pCVDZ', 'd-aug-cc-pCVTZ', 'd-aug-cc-pCVQZ']  # , 'd-aug-cc-pV5Z', 'd-aug-cc-pV6Z']
+basis_list = ['aug-cc-pCVDZ', 'aug-cc-pCVTZ', 'aug-cc-pCVQZ', 'aug-cc-pV5Z']  # , 'aug-cc-pV5Z', 'aug-cc-pV6Z']
+d_basis_list = ['d-aug-cc-pCVDZ', 'd-aug-cc-pCVTZ', 'd-aug-cc-pCVQZ',
+                'd-aug-cc-pV5Z']  # , 'd-aug-cc-pV5Z', 'd-aug-cc-pV6Z']
 
 runner = DaltonRunner()
 
@@ -20,6 +20,3 @@ for basis in basis_list + d_basis_list:
         result = runner.get_polar_json(mol, 'hf', 'dipole', basis)
     except:
         pass
-
-
-
