8842900a8fda5c8462ea26bb02a9bd289faad211
Adrian Hurtado <adrian.hurtado@stonybrook.edu>
2022-08-17 12:15:27 -0400

    sarate pc and regular

--- compute_dalton_basis_pc.py
+++ compute_dalton_basis_pc.py
@@ -7,10 +7,11 @@ from daltonRunner import DaltonRunner
 mol = sys.argv[1]
 num_proc = sys.argv[2]
 
-basis_list = ['aug-cc-pV5Z']  # , 'aug-cc-pV5Z', 'aug-cc-pV6Z']
-d_basis_list = ['d-aug-cc-pV5Z']  # , 'd-aug-cc-pV5Z', 'd-aug-cc-pV6Z']
+basis_list = ['aug-cc-pCVDZ', 'aug-cc-pCVTZ', 'aug-cc-pCVQZ']  # , 'aug-cc-pV5Z', 'aug-cc-pV6Z']
+d_basis_list = ['d-aug-cc-pCVDZ', 'd-aug-cc-pCVTZ', 'd-aug-cc-pCVQZ']
 
 runner = DaltonRunner()
+
 runner.Np = num_proc
 
 for basis in basis_list + d_basis_list:
